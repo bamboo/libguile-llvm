a6a6abee0d9d6ebb6a9a595e833bcbb32d53e6e9
Rodrigo B. de Oliveira <rbo@acm.org>
2014-12-12 13:56:35 -0200

    emit complete factorial function

--- check.scm
+++ check.scm
@@ -1,27 +1,64 @@
-(use-modules
- (llvm)
- (srfi srfi-1))
+(use-modules (llvm))
 
-(define (->type-array types)
-  (let* [(size (length types))
-         (array (LLVMTypeArrayMake size))]
-    (for-each
-     (lambda (index-and-value)
-       (LLVMTypeArraySet array (car index-and-value) (cdr index-and-value)))
-     (map cons (iota size) types))
-    array))
+(define (->llvm-array constructor setter values)
+  (let* [(size (length values))
+         (array (constructor size))]
+    (do [(i 0 (1+ i))
+         (values values (cdr values))]
+        ((= i size)
+         array)
+      (setter array i (car values)))))
 
-(define (type-array . types)
-  (->type-array types))
+(define (->llvm-type-array values)
+  (->llvm-array LLVMTypeArrayMake LLVMTypeArraySet values))
+
+(define (llvm-type-array . values)
+  (->llvm-type-array values))
+
+(define (->llvm-value-array values)
+  (->llvm-array LLVMValueArrayMake LLVMValueArraySet values))
+
+(define (llvm-value-array . values)
+  (->llvm-value-array values))
+
+(define (llvm-block-array . values)
+  (->llvm-array LLVMBasicBlockArrayMake LLVMBasicBlockArraySet values))
 
 (LLVMInitializeNativeTarget)
 
 (define mod (LLVMModuleCreateWithName "fac_module"))
-(define int32 (LLVMInt32Type))
-(define args (type-array int32))
-(define fac (LLVMAddFunction mod "fac" (LLVMFunctionType int32 args 1 #f)))
+(define i32 (LLVMInt32Type))
+(define args (llvm-type-array i32))
+(define fac (LLVMAddFunction mod "fac" (LLVMFunctionType i32 args 1 #f)))
 (LLVMSetFunctionCallConv fac (LLVMCCallConv))
 
-;(define n (LLVMGetParam fac 0))
+(define n (LLVMGetParam fac 0))
+
+(define entry (LLVMAppendBasicBlock fac "entry"))
+(define iftrue (LLVMAppendBasicBlock fac "iftrue"))
+(define iffalse (LLVMAppendBasicBlock fac "iffalse"))
+(define end (LLVMAppendBasicBlock fac "end"))
+
+(define builder (LLVMCreateBuilder))
+(LLVMPositionBuilderAtEnd builder entry)
+
+(define If (LLVMBuildICmp builder (LLVMIntEQ) n (LLVMConstInt i32 0 0) "n == 0"))
+(LLVMBuildCondBr builder If iftrue iffalse)
+(LLVMPositionBuilderAtEnd builder iftrue)
+(define res_iftrue (LLVMConstInt i32 1 0))
+(LLVMBuildBr builder end)
+(LLVMPositionBuilderAtEnd builder iffalse)
+(define n_minus (LLVMBuildSub builder n (LLVMConstInt i32 1 0) "n - 1"))
+
+(define call_fac_args (llvm-value-array n_minus))
+(define call_fac (LLVMBuildCall builder fac call_fac_args 1 "fac(n - 1)"))
+(define res_iffalse (LLVMBuildMul builder n call_fac "n * fac(n - 1)"))
+(LLVMBuildBr builder end)
+(LLVMPositionBuilderAtEnd builder end)
+(define res (LLVMBuildPhi builder i32 "result"))
+(define phi_vals (llvm-value-array res_iftrue res_iffalse))
+(define phi_blocks (llvm-block-array iftrue iffalse))
+(LLVMAddIncoming res phi_vals phi_blocks 2)
+(LLVMBuildRet builder res)
 
 (LLVMDumpModule mod)
